794c5318e454b7bfc414aa4708dc9c7481aa6b3a
Amira AYADI <ayadi.amira01@gmail.com>
2017-03-03 11:21:26 +0100

    Maj Parser.py
    Mise a jour

--- Parser.py
+++ Parser.py
@@ -2,20 +2,17 @@ import requests
 from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine , LTImage , LTFigure, LTCurve
 import io
 import os
 import re
 import time
 
-
-dossierPDF = 
-
-lien="http://iopscience.iop.org/article/10.1209/0295-5075/112/42001/pdf"
-print(lien)
-search = urllib.request.urlopen(lien).read()
-memoire= io.BytesIO(search)
-parser = PDFParser(memoire)
+ListePDF = os.listdir("PDF/")
+for filename in ListePDF[:1] :
+    print(filename)
+fn = open("PDF/"+filename, 'rb')
+parser = PDFParser(fn)
 doc = PDFDocument()
 parser.set_document(doc)
 doc.set_parser(parser)
@@ -31,6 +28,7 @@ for page in doc.get_pages():
         if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
             print(lt_obj.get_text())
 
-        if isinstance(lt_obj, LTImage) or isinstance(lt_obj,LTFigure) or isinstance(lt_obj,LTCurve):
+        if isinstance(lt_obj, LTImage) or isinstance(lt_obj,LTFigure) :
             print(lt_obj)
-            save_image(lt_obj)
+
+    
