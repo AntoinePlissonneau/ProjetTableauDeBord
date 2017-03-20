de16b9992cc8f61d1f84e3254bdd9272aadd933b
AntoinePlissonneau <plissonneau.antoine@gmail.com>
2017-03-20 18:38:32 +0100

    no message

--- Parser.py
+++ Parser.py
@@ -44,21 +44,30 @@ for page in doc.get_pages():
 
 #Extraction des légendes des figures pour chaque page
 ok=list()
-caption=dict()
+
 for i in range(1,numPage):
     for elem in txtPage[i]:
+        caption=dict()
         match = re.search("^Figure [0-9]+\.", elem) 
         if match:
-            caption['numFigure']=re.sub(r'^Figure ([0-9])+\..*',r'\1', elem)
+            caption['numFigure']=re.sub(r'^Figure ([0-9])+\.(.|\n)*',r'\1', elem)
             caption['Figure'] = elem
             caption['Page'] = i
-            print(caption)
             ok.append(caption)
-            print("\n")
-            print(ok)
+            
+        
 
 #Extraction des références directes aux figures
-
+#refList=list()
+#ref=dict()
+#for i in range(1,numPage):
+#    for elem in txtPage[i]:
+#        match = re.search("(F|f)igure [0-9]+", elem) 
+#        if match and (elem not in [item['Figure'] for item in ok]):
+#            num=re.sub(r'(F|f)igure ([0-9])+(.|\n)*',r'\2', elem)
+#            [item['Figure'] for item in ok]
+#            print(elem)
+#            print(num)
 
 
 #Im = Image, Fm= font?
