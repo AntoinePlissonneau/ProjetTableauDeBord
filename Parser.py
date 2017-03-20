56290e87f304d860b8c8edd296eba62ed4847339
AntoinePlissonneau <plissonneau.antoine@gmail.com>
2017-03-20 19:36:38 +0100

    no message

--- Parser.py
+++ Parser.py
@@ -53,21 +53,31 @@ for i in range(1,numPage):
             caption['numFigure']=re.sub(r'^Figure ([0-9])+\.(.|\n)*',r'\1', elem)
             caption['Figure'] = elem
             caption['Page'] = i
+            caption['Ref'] = []
             ok.append(caption)
             
         
 
 #Extraction des références directes aux figures
-#refList=list()
-#ref=dict()
-#for i in range(1,numPage):
-#    for elem in txtPage[i]:
-#        match = re.search("(F|f)igure [0-9]+", elem) 
-#        if match and (elem not in [item['Figure'] for item in ok]):
-#            num=re.sub(r'(F|f)igure ([0-9])+(.|\n)*',r'\2', elem)
-#            [item['Figure'] for item in ok]
-#            print(elem)
-#            print(num)
+
+
+for i in range(1,numPage):
+    for elem in txtPage[i]:
+        ref=dict()
+        match = re.search("(F|f)igure [0-9]+", elem) 
+        if match and (elem not in [item['Figure'] for item in ok]):
+            findAll = re.findall("[F|f]igure [0-9]+", elem) 
+            print(findAll)
+            refList=list()
+            for fig in findAll: 
+                num=re.sub(r'(.|\n)*([0-9])+',r'\2', fig)
+                print(num)
+                relatedFigure = [item for item in ok if item['numFigure'] == num]
+                numIndex= ok.index(relatedFigure[0])
+                ref['Ref'] = elem
+                ref['Page'] = i
+                ok[numIndex]['Ref'].append(ref)
+            
 
 
 #Im = Image, Fm= font?
